d5d1b6fc12d9efc3188733deb9c7397283b8de93
Marko Mahnič <marko.mahnic@gmail.com>
2018-12-14 18:37:32 +0100

    Ad and initial implementation of parseArguments

--- src/argparser.h
+++ src/argparser.h
@@ -8,8 +8,11 @@ class CArgumentParser
 public:
    class CParameter
    {
+      friend class CArgumentParser;
+   private:
       std::string& mValue;
       std::string mShortName;
+
    public:
       CParameter( std::string& value )
          : mValue( value )
@@ -34,5 +37,26 @@ public:
 
    void parseArguments( const std::vector<std::string>& args )
    {
+      int iparam = -1;
+      for ( auto& arg : args ) {
+         std::string name;
+         if ( arg.substr( 0, 2 ) == "--" )
+            name = arg.substr( 2 );
+         else if ( arg.substr( 0, 1 ) == "-" )
+            name = arg.substr( 1 );
+
+         if ( name.empty() ) {
+            if ( iparam >= 0 )
+               mParameters[iparam].mValue = arg;
+         }
+         else {
+            iparam = -1;
+            for ( int i = 0; i < mParameters.size(); ++i ) {
+               auto& param = mParameters[i];
+               if ( param.mShortName == name )
+                  iparam = i;
+            }
+         }
+      }
    }
 };
