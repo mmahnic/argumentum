68f60c9fd6cebee32b1fd6181671f4493f253d37
Marko Mahnič <marko.mahnic@gmail.com>
2019-02-21 21:49:42 +0100

    Verify that the value can be modified directly by an action

--- test/action_t.cpp
+++ test/action_t.cpp
@@ -59,3 +59,32 @@ TEST( ArgumentParserActionTest, shouldNotSetValueIfActionReturnsEmptyOptional )
    EXPECT_TRUE( result.empty() );
 }
 
+TEST( ArgumentParserActionTest, shouldSetValueOnTargetFromAction )
+{
+   class TestAction: public argparse::ArgumentParser::Action
+   {
+      std::optional<std::string> exec( argparse::ArgumentParser::Value& target,
+            const std::string &value ) override
+      {
+         if ( value.find( "1" ) != std::string::npos )
+            target.setValue( value + " Has One" );
+         else
+            target.setValue( value );
+         return {};
+      }
+   };
+
+   std::string result;
+   std::stringstream strout;
+   auto parser = ArgumentParser::create();
+   parser.add_argument( result, "-v" ).maxargs(1).action( std::make_shared<TestAction>() );
+
+   auto res = parser.parse_args( { "-v", "31" } );
+   EXPECT_TRUE( res.errors.empty() );
+   EXPECT_EQ( "31 Has One", result );
+
+   res = parser.parse_args( { "-v", "2" } );
+   EXPECT_TRUE( res.errors.empty() );
+   EXPECT_EQ( "2", result );
+}
+
