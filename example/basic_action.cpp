f08a650a90d488894b869600f89a490b5c4f6022
Marko Mahnič <marko.mahnic@gmail.com>
2019-10-21 20:28:28 +0200

    Fix basic_action example by defining the default value

--- example/basic_action.cpp
+++ example/basic_action.cpp
@@ -9,27 +9,25 @@ using namespace argparse;
 int main( int argc, char** argv )
 {
    vector<int> numbers;
-   auto omax = []( int a, int b ) { return std::max( a, b ); };
-   auto osum = []( int a, int b ) { return a + b; };
-   std::function<int( int, int )> operation = omax;
-   int init = INT_MIN;
+   auto max = []( int a, int b ) { return std::max( a, b ); };
+   auto sum = []( int a, int b ) { return a + b; };
+   std::pair<std::function<int( int, int )>, int> operation;
 
    auto parser = argument_parser{};
    parser.config().program( argv[0] ).description( "Accumulator" );
    parser.add_argument( numbers, "N" ).minargs( 1 ).metavar( "INT" ).help( "Integers" );
    parser.add_argument( operation, "--sum", "-s" )
          .nargs( 0 )
+         .absent( std::make_pair( max, INT_MIN ) )
          .action( [&]( auto& target, const std::string& value ) {
-            target = osum;
-            init = 0;
+            target = std::make_pair( sum, 0 );
          } )
          .help( "Sum the integers (default: find the max)" );
 
    if ( !parser.parse_args( argc, argv, 1 ) )
       return 1;
 
-   std::cout << "parsed\n";
-   auto acc = accumulate( numbers.begin(), numbers.end(), init, operation );
+   auto acc = accumulate( numbers.begin(), numbers.end(), operation.second, operation.first );
    cout << acc << "\n";
    return 0;
 }
