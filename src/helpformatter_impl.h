5dad465c13f2562897b59e8e278f006460245c7b
Marko Mahnič <marko.mahnic@gmail.com>
2019-11-03 17:34:17 +0100

    Move option sorter to a separate file

--- src/helpformatter_impl.h
+++ src/helpformatter_impl.h
@@ -6,75 +6,10 @@
 #include "helpformatter.h"
 
 #include "argparser.h"
+#include "optionsorter.h"
 #include "writer.h"
 
 namespace argparse {
-
-class OptionSorter
-{
-public:
-   struct GroupLimit
-   {
-      using iterator_t = std::vector<ArgumentHelpResult>::iterator;
-      // Start of group in the reordered ArgumentHelpResult vector.
-      iterator_t ibegin;
-      // End of positional parameters.
-      iterator_t iendpos;
-      // End of requiered parameters.
-      iterator_t iendreq;
-      // End of optional parameters, end of group.
-      iterator_t iend;
-      GroupLimit( iterator_t begin, iterator_t end )
-         : ibegin( begin )
-         , iendpos( begin )
-         , iendreq( begin )
-         , iend( end )
-      {}
-   };
-
-   // 1. required free group
-   // 2. required groups by name
-   // 3. optional free group
-   // 4. optional groups by name
-   // 5. commands
-   std::vector<GroupLimit> reorderGroups( std::vector<ArgumentHelpResult>& args )
-   {
-      auto lowerGroup = []( auto&& a, auto&& b ) {
-         if ( a.isCommand == b.isCommand ) {
-            if ( a.group.isRequired == b.group.isRequired )
-               return a.group.name < b.group.name;
-
-            // Required before optional ( false < true => !isRequired(true) < isRequired(true) )
-            return !a.group.isRequired < !b.group.isRequired;
-         }
-
-         return a.isCommand < b.isCommand;
-      };
-
-      std::stable_sort( std::begin( args ), std::end( args ), lowerGroup );
-
-      std::vector<GroupLimit> limits;
-      auto iprev = std::begin( args );
-      for ( auto icur = iprev; icur != std::end( args ); ++icur ) {
-         if ( lowerGroup( *iprev, *icur ) ) {
-            limits.emplace_back( iprev, icur );
-            iprev = icur;
-         }
-      }
-      if ( iprev != std::end( args ) )
-         limits.emplace_back( iprev, std::end( args ) );
-
-      return limits;
-   }
-
-   void reorderOptions( GroupLimit& limit )
-   {
-      limit.iendpos = std::stable_partition(
-            limit.ibegin, limit.iend, []( auto&& opt ) { return opt.is_positional(); } );
-      limit.iendreq = std::stable_partition(
-            limit.iendpos, limit.iend, []( auto&& opt ) { return opt.is_required(); } );
-   }
-};
 
 inline void HelpFormatter::formatUsage(
       const argument_parser& parser, std::vector<ArgumentHelpResult>& args, Writer& writer )
