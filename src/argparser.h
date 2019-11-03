9f8cd7062c0e886781e5f17c60fc8162a8ae10bd
Marko Mahnič <marko.mahnic@gmail.com>
2019-11-03 12:10:00 +0100

    Move exceptions to a separate file

--- src/argparser.h
+++ src/argparser.h
@@ -4,6 +4,7 @@
 #pragma once
 
 #include "convert.h"
+#include "exceptions.h"
 #include "helpformatter_i.h"
 
 #include <algorithm>
@@ -19,57 +20,6 @@
 #include <vector>
 
 namespace argparse {
-
-class InvalidChoiceError : public std::invalid_argument
-{
-public:
-   InvalidChoiceError( const std::string& value )
-      : std::invalid_argument( value )
-   {}
-};
-
-class UncheckedParseResult : public std::exception
-{
-public:
-   const char* what() const noexcept override
-   {
-      return "Unchecked parse result.";
-   }
-};
-
-class MixingGroupTypes : public std::runtime_error
-{
-public:
-   MixingGroupTypes( const std::string& groupName )
-      : runtime_error( std::string( "Mixing group types in group '" ) + groupName + "'." )
-   {}
-};
-
-class RequiredExclusiveOption : public std::runtime_error
-{
-public:
-   RequiredExclusiveOption( const std::string& groupName, const std::string& optionName )
-      : runtime_error( std::string( "Option '" ) + optionName + "' is required in exclusive group '"
-              + groupName + "'." )
-   {}
-};
-
-class DuplicateOption : public std::runtime_error
-{
-public:
-   DuplicateOption( const std::string& groupName, const std::string& optionName )
-      : runtime_error( std::string( "Option '" ) + optionName + "' is already defined in group '"
-              + groupName + "'." )
-   {}
-};
-
-class DuplicateCommand : public std::runtime_error
-{
-public:
-   DuplicateCommand( const std::string& commandName )
-      : runtime_error( std::string( "Command '" ) + commandName + "' is already defined." )
-   {}
-};
 
 class argument_parser;
 
