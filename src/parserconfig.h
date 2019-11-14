ec88e5aaa81d37b135ad202630b35d773947f9ad
Marko Mahnič <marko.mahnic@gmail.com>
2019-11-14 21:55:34 +0100

    Describe parserconfig setters

--- src/parserconfig.h
+++ src/parserconfig.h
@@ -25,35 +25,42 @@ private:
    Data mData;
 
 public:
+   // Used internally to access the configured parameters.
    const Data& data() const
    {
       return mData;
    }
 
+   // Set the program name to be used in the generated help.
    ParserConfig& program( std::string_view program )
    {
       mData.program = program;
       return *this;
    }
 
+   // Set the usage string to be used in the generated help. This overrides the
+   // default generated usage string.
    ParserConfig& usage( std::string_view usage )
    {
       mData.usage = usage;
       return *this;
    }
 
+   // Set the description of the program to be shown in the generated help.
    ParserConfig& description( std::string_view description )
    {
       mData.description = description;
       return *this;
    }
 
+   // Set the epolog to be shown at the end of the generated help.
    ParserConfig& epilog( std::string_view epilog )
    {
       mData.epilog = epilog;
       return *this;
    }
 
+   // Set the stream to which the parser will write messages.
    // NOTE: The @p stream must outlive the parser.
    ParserConfig& cout( std::ostream& stream )
    {
