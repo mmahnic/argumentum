7e79372bb5e85591846dce0806ab655113bb17c2
Marko Mahnič <marko.mahnic@gmail.com>
2019-11-14 21:50:47 +0100

    Describe groupconfig and commandconfig setters

--- src/groupconfig.h
+++ src/groupconfig.h
@@ -16,18 +16,23 @@ public:
       : mpGroup( pGroup )
    {}
 
+   // Set the title of the group that will be displayed in the generated help.
    GroupConfig& title( std::string_view title )
    {
       mpGroup->setTitle( title );
       return *this;
    }
 
+   // Set the description of the group that will be displayed in the generated
+   // help.
    GroupConfig& description( std::string_view description )
    {
       mpGroup->setDescription( description );
       return *this;
    }
 
+   // Set to true if at least one option from the group must be present in the
+   // input arguments.
    GroupConfig& required( bool isRequired = true )
    {
       mpGroup->setRequired( isRequired );
