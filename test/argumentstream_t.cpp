240e6241550da82eb35cce877292815681981431
Marko Mahnič <marko.mahnic@gmail.com>
2019-11-16 12:15:45 +0100

    Add a test for a stream of arguments

---
+++ test/argumentstream_t.cpp
@@ -0,0 +1,23 @@
+// Copyright (c) 2018, 2019 Marko Mahnič
+// License: MPL2. See LICENSE in the root of the project.
+
+#include "../src/argparser.h"
+
+#include <gtest/gtest.h>
+
+using namespace argparse;
+
+TEST( ArgumentStream, shouldUseIteratorsAsAStream )
+{
+   std::vector<std::string> args{ "one", "two", "three" };
+
+   ArgumentStream stream( args.begin(), args.end() );
+   std::vector<std::string> res;
+   for ( auto arg = stream.next(); !!arg; arg = stream.next() )
+      res.push_back( *arg );
+
+   ASSERT_EQ( args.size(), res.size() );
+
+   for ( unsigned i = 0; i < args.size(); ++i )
+      EXPECT_EQ( args[i], res[i] );
+}
