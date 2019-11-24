05356c1d27039b5b345c233ac7b8775212d0d0a6
Marko Mahnič <marko.mahnic@gmail.com>
2019-11-24 13:43:42 +0100

    Split option sorter into header and include files

--- include/cppargparse/argparse.h
+++ include/cppargparse/argparse.h
@@ -16,6 +16,7 @@
 #include "../../src/helpformatter_impl.h"
 #include "../../src/optionconfig_impl.h"
 #include "../../src/options_impl.h"
+#include "../../src/optionsorter_impl.h"
 #include "../../src/parser_impl.h"
 #include "../../src/parserdefinition_impl.h"
 #include "../../src/values_impl.h"
--- src/optionsorter.h
+++ src/optionsorter.h
@@ -5,7 +5,6 @@
 
 #include "helpformatter.h"
 
-#include <algorithm>
 #include <vector>
 
 namespace argparse {
@@ -32,48 +31,8 @@ public:
       {}
    };
 
-   // 1. required free group
-   // 2. required groups by name
-   // 3. optional free group
-   // 4. optional groups by name
-   // 5. commands
-   std::vector<GroupLimit> reorderGroups( std::vector<ArgumentHelpResult>& args )
-   {
-      auto lowerGroup = []( auto&& a, auto&& b ) {
-         if ( a.isCommand == b.isCommand ) {
-            if ( a.group.isRequired == b.group.isRequired )
-               return a.group.name < b.group.name;
-
-            // Required before optional ( false < true => !isRequired(true) < isRequired(true) )
-            return !a.group.isRequired < !b.group.isRequired;
-         }
-
-         return a.isCommand < b.isCommand;
-      };
-
-      std::stable_sort( std::begin( args ), std::end( args ), lowerGroup );
-
-      std::vector<GroupLimit> limits;
-      auto iprev = std::begin( args );
-      for ( auto icur = iprev; icur != std::end( args ); ++icur ) {
-         if ( lowerGroup( *iprev, *icur ) ) {
-            limits.emplace_back( iprev, icur );
-            iprev = icur;
-         }
-      }
-      if ( iprev != std::end( args ) )
-         limits.emplace_back( iprev, std::end( args ) );
-
-      return limits;
-   }
-
-   void reorderOptions( GroupLimit& limit )
-   {
-      limit.iendpos = std::stable_partition(
-            limit.ibegin, limit.iend, []( auto&& opt ) { return opt.is_positional(); } );
-      limit.iendreq = std::stable_partition(
-            limit.iendpos, limit.iend, []( auto&& opt ) { return opt.is_required(); } );
-   }
+   std::vector<GroupLimit> reorderGroups( std::vector<ArgumentHelpResult>& args );
+   void reorderOptions( GroupLimit& limit );
 };
 
 }   // namespace argparse
