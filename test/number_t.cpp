4eca4b61aac79b75e6a008bccb67c3e7e53334f2
Marko Mahnič <marko.mahnic@gmail.com>
2019-12-03 22:11:01 +0100

    Add a test for parsing integers with 0d prefix

---
+++ test/number_t.cpp
@@ -0,0 +1,22 @@
+// Copyright (c) 2018, 2019 Marko Mahnič
+// License: MPL2. See LICENSE in the root of the project.
+
+#include <cppargparse/argparse-s.h>
+
+#include <algorithm>
+#include <gtest/gtest.h>
+#include <vector>
+
+using namespace argparse;
+using namespace testing;
+
+TEST( NumberTest, shouldParseIntegerWithDecimalPrefix )
+{
+   int d;
+   auto parser = argument_parser{};
+   parser.add_argument( d, "number" ).nargs( 1 );
+
+   auto res = parser.parse_args( { "0d12" } );
+   EXPECT_TRUE( static_cast<bool>( res ) );
+   EXPECT_EQ( 12, d );
+}
