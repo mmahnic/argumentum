f8c2907c37a6b626b0a43872a2860b44dda7432c
Marko Mahnič <marko.mahnic@gmail.com>
2019-12-26 10:59:50 +0100

    Improve cmake files so that import targets are generated
    The static library can be linked by using the target
    Argumenutm::argparse-s.

--- InstallConfig.cmake
+++ InstallConfig.cmake
@@ -1,20 +1,35 @@
 
 include(CMakePackageConfigHelpers)
 
-configure_package_config_file(cmake/ArgumentumConfig.cmake.in
-   ${CMAKE_CURRENT_BINARY_DIR}/ArgumentumConfig.cmake
-   INSTALL_DESTINATION ${LIB_INSTALL_DIR}/Argumentum/cmake
-   PATH_VARS INCLUDE_INSTALL_DIR LIB_INSTALL_DIR )
+set( cmake_package_name ${PROJECT_NAME} )
+set( package_version ${PROJECT_VERSION} )
 
-write_basic_package_version_file(
-   ${CMAKE_CURRENT_BINARY_DIR}/ArgumentumConfigVersion.cmake
-   VERSION ${PROJECT_VERSION}
+set( generated_dir "${CMAKE_CURRENT_BINARY_DIR}/gen" CACHE INTERNAL "" )
+set( cmake_files_install_dir "${CMAKE_INSTALL_LIBDIR}/cmake/${cmake_package_name}" )
+set( version_file "${generated_dir}/${cmake_package_name}ConfigVersion.cmake" )
+set( config_file_in "cmake/ArgumentumConfig.cmake.in" )
+set( config_file "${generated_dir}/${cmake_package_name}Config.cmake" )
+set( targets_export_name ${cmake_package_name}Targets CACHE INTERNAL "" )
+
+write_basic_package_version_file( ${version_file}
+   VERSION ${package_version}
    COMPATIBILITY AnyNewerVersion
    )
 
-install(FILES
-   ${CMAKE_CURRENT_BINARY_DIR}/ArgumentumConfig.cmake
-   ${CMAKE_CURRENT_BINARY_DIR}/ArgumentumConfigVersion.cmake
-   DESTINATION ${CONFIG_INSTALL_DIR}
+install(
+   EXPORT ${targets_export_name}
+   NAMESPACE ${cmake_package_name}::
+   DESTINATION ${cmake_files_install_dir}
+   )
+
+configure_package_config_file(
+   ${config_file_in}
+   ${config_file}
+   INSTALL_DESTINATION ${cmake_files_install_dir}
+   )
+
+install(
+   FILES ${version_file} ${config_file}
+   DESTINATION ${cmake_files_install_dir}
    )
 
