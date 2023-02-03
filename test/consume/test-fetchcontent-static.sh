0e954d358f522345226da91889d1610bcc67355d
Marko Mahnič <marko.mahnic@gmail.com>
2023-02-03 19:07:22 +0100

    Add a test for consuming a static library with FetchContent

---
+++ test/consume/test-fetchcontent-static.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+here=$(pwd)
+workdir=xdata/test-fetchcontent-static
+argumentumgit="file://$(realpath ../../../argumentum)"
+builddir=out/build
+
+if [ -d $workdir ]; then
+   rm -rf $workdir
+fi
+
+if [ ! -d $workdir ]; then
+   mkdir -p $workdir
+fi
+
+create_test_git() {
+   cp .gitignore $workdir/
+   sed -e "s#@THIS_REPO@#$argumentumgit#" CMakeLists-fetchcontent.txt.in > $workdir/CMakeLists.txt
+   cp -r src $workdir/
+   cd $workdir
+
+   mkdir -p $builddir
+
+   git init
+   git add .gitignore
+   git commit -m "Add gitignore"
+   git add src
+   git commit -m "Initial import"
+}
+
+configure() {
+   # local debug=--debug-output
+   cmake -S . -B $builddir -D ARGUMENTUM_BUILD_STATIC_LIBS=ON $debug
+}
+
+build() {
+   # local debug=--debug-output
+   cmake --build $builddir $debug
+}
+
+git config --global protocol.file.allow always
+create_test_git
+configure
+build
+git config --global --unset protocol.file.allow
+
