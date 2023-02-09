bae1d59ab740927d2b93dd4f82d230d62aa00f3a
Marko Mahnič <marko.mahnic@gmail.com>
2023-02-09 19:41:52 +0100

    Do not publish the static-lib header if static lib is not published

--- include/prepare-headers.cmake
+++ include/prepare-headers.cmake
@@ -1,5 +1,5 @@
 # Run with cmake -P
-# Parameters (-D): TOP_SOURCE_DIR, P_BINARY_DIR, P_SOURCE_DIR, P_HEADERONLY
+# Parameters (-D): TOP_SOURCE_DIR, P_BINARY_DIR, P_SOURCE_DIR, P_HEADERONLY, P_BUILDSTATIC
 
 file( MAKE_DIRECTORY ${P_BINARY_DIR}/argumentum )
 
@@ -8,14 +8,16 @@ file( COPY ${TOP_SOURCE_DIR}/src/
    FILES_MATCHING PATTERN "*.h"
    )
 
-file( READ ${P_SOURCE_DIR}/argumentum/argparse.h
-   main_header )
+if( P_BUILDSTATIC )
+   file( READ ${P_SOURCE_DIR}/argumentum/argparse.h
+      main_header )
 
-string( REPLACE "../../src/" "inc/"
-   main_header "${main_header}" )
+   string( REPLACE "../../src/" "inc/"
+      main_header "${main_header}" )
 
-file( WRITE ${P_BINARY_DIR}/argumentum/argparse.h
-   "${main_header}" )
+   file( WRITE ${P_BINARY_DIR}/argumentum/argparse.h
+      "${main_header}" )
+endif()
 
 if( P_HEADERONLY )
    file( READ ${P_SOURCE_DIR}/argumentum/argparse-h.h
