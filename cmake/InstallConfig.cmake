5ee1aace171f2a8ed3d04b1a75141dbb364699c4
Marko Mahnič <marko.mahnic@gmail.com>
2023-02-09 19:53:46 +0100

    Do not install configuration files if no targets need to be exported

--- cmake/InstallConfig.cmake
+++ cmake/InstallConfig.cmake
@@ -11,25 +11,26 @@ set( config_file_in "cmake/ArgumentumConfig.cmake.in" )
 set( config_file "${generated_dir}/${cmake_package_name}Config.cmake" )
 set( targets_export_name ${cmake_package_name}Targets CACHE INTERNAL "" )
 
-write_basic_package_version_file( ${version_file}
-   VERSION ${package_version}
-   COMPATIBILITY AnyNewerVersion
-   )
+if ( _argumentum_has_exported_targets )
+   write_basic_package_version_file( ${version_file}
+      VERSION ${package_version}
+      COMPATIBILITY AnyNewerVersion
+      )
 
-install(
-   EXPORT ${targets_export_name}
-   NAMESPACE ${cmake_package_name}::
-   DESTINATION ${cmake_files_install_dir}
-   )
+   install(
+      EXPORT ${targets_export_name}
+      NAMESPACE ${cmake_package_name}::
+      DESTINATION ${cmake_files_install_dir}
+      )
 
-configure_package_config_file(
-   ${config_file_in}
-   ${config_file}
-   INSTALL_DESTINATION ${cmake_files_install_dir}
-   )
+   configure_package_config_file(
+      ${config_file_in}
+      ${config_file}
+      INSTALL_DESTINATION ${cmake_files_install_dir}
+      )
 
-install(
-   FILES ${version_file} ${config_file}
-   DESTINATION ${cmake_files_install_dir}
-   )
-
+   install(
+      FILES ${version_file} ${config_file}
+      DESTINATION ${cmake_files_install_dir}
+      )
+endif()
